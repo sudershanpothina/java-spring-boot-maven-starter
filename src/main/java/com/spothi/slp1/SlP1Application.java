a20680ebf21e58fdedce7aa346e5b638788b69f8
unknown <sudershanpothina@gmail.com>
2021-03-06 12:02:07 -0600

    initial commit

---
+++ src/main/java/com/spothi/slp1/SlP1Application.java
@@ -0,0 +1,13 @@
+package com.spothi.slp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlP1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlP1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/spothi/slp1/SlP1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spothi.slp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlP1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
